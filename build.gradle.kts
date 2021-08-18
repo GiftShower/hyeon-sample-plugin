404eed52b24a30e4ede7f1b8130d2f15656f08ab
GiftShower_ <58767497+GiftShower@users.noreply.github.com>
2021-08-18 16:28:24 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     compileOnly(kotlin("stdlib")) // Kotlin
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT") // Paper Latest
     compileOnly("io.github.monun:tap:4.0.0-RC")
-    compileOnly("io.github.monun:kommand:1.2.1") // 와 shadowJar 버리는 시대가 올 줄이야
+    compileOnly("io.github.monun:kommand-api:2.6.4") // 와 shadowJar 버리는 시대가 올 줄이야
 }
 
 tasks { // 아니 시발 페이퍼 개놈들이 굳이 꼭 16만 되게 해야할 이유가 뭔데요
